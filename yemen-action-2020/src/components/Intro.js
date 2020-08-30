25851d2fec120c569123aa1a487cb0fe781c656a
Drifa Atladottir <drifaatladottir@Drifas-MacBook-Pro.local>
2020-08-30 19:24:14 +0000

    fyrstu drög f. intro/header

--- yemen-action-2020/src/components/Intro.js
+++ yemen-action-2020/src/components/Intro.js
@@ -5,11 +5,11 @@ class Intro extends Component {
   render() {
     return (
     <div className="Intro">
-      <div className="Triangle">
-      </div>
       <div className="IntroText">
         <h1>Deyja úr hungri</h1>
         <h2>Börn í Jemen þurfa þína hjálp</h2>      
+      </div>
+      <div className="Triangle">
       </div>
     </div>
     );
