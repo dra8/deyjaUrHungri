2f1e890ed011087a3224c745af6e14164d4db8f3
Drifa Atladottir <drifaatladottir@Drifas-MacBook-Pro.local>
2020-08-31 13:06:53 +0000

    bæti við animation

--- yemen-action-2020/src/components/About.js
+++ yemen-action-2020/src/components/About.js
@@ -6,14 +6,14 @@ class About extends Component {
     return (
       <div className="about">
         <p className="aboutText">Í Jemen hefur geysað borgarastyrjöld síðan 2015. Samkvæmt tölum Amnesty International eru um 16 milljónir mans sem lifa við sára hungursneið í Jemen. Borgarastyrjöldinni fylgir ekki einungis hungursneið, en á þeim rúmlega fimm árum sem hún hefur geysað er talið að 230 þúsund almennir borgarar hafi látist, en 20 þúsund dauðsfallanna hafa verið staðfest af Mannréttindastofnun Sameinuðu Þjóðanna. Styrjöldinni fylgja alvarleg mannréttindabrot, sem oft geta talist alvarlegir stríðsglæpir.</p>
-
+        <div className="shape">
+        </div>
         <button className="moreButton">Lesa meira ></button>
 
         <p className="aboutMoreText"> Árið 2011 var forsetanum Ali Abdullah Saleh steypt af stóli eftir 33 ára valdatíð. Hann hafði lengi verið ásakaðum um alvarleg spillingarbrot, þá einkum af Huthi hópnum, sem eru samtök vopnaðra Zaidista. Abd Rabbu Mansour Hadi tók við af Saleh, en hann hafði starfað fyrir ríkisstjórn hans. Eftir tvö ár við völd lagði Hadi fram breytingar á stjórnarskipulagi í Jemen, sem skipti landinu í fylki, án tillits til samfélagslegra aðstæðna í hverju svæði fyrir sig. Þessari áætlun fygldi mikið mótlæti, þar á meðal frá Huthi samtökunum. Samtökin nýttu sér óánægju ríkisborgara Jemen og hófu yfirtöku nálægra svæði í Norðri, svo sem í Sa´da borg, í September 2014. Í byrjun árs 2015 náðu þeir völdum yfir höfuðborginni Sana’a og þurfti sytjandi forseti og hans stjórn að flýja. Hadi forseti og hans stjórn fékk Saudi Arabíu og Sameinuðu Arabísku Furstadæmin með sér í lið til þess að ná aftur völdum og þann 25 Mars 2015 hófst styrjöldin sem enn stendur yfir. Hún nær nú ekki aðeins yfir höfuðborgina og norðurhluta landsins eins og til að byrja með, heldur hafa átökin heltekið alla þjóðina. Átökunum er langt frá því að linna og hafa margir Jemenar þurft að flýja heimili sín vegna hungurs, fátæktar eða eyðileggingar. 
 
         Samkvæmt tölum Sameinuðu Þjóðanna gætu 2.4 milljónir barna í Jemen orðið vannærð í lok árs, helmingur þeirra undir 5 ára að aldri. Auk þess er talið að 30þúsund börn til viðbótar muni þróa með sér alvarlega sjúkdóma vegna mikillar vannæringar á næsta hálfa ári. Covid-19 faraldurinn hefur verið einstaklega skæður í Jemen þar sem margir íbúar hafa ekki aðgang að heilbrigðisþjónustu. Erfitt hefur reynst að prófa fyrir sjúkdóminum í Jemen en talið er að sjúkdómurinn sé mun útbreiddari en tölur gefa til kynna. Samkvæmt yfirlýsingu UNICEF hefur faraldurinn ýtt undir mörg vandamál sem voru þegar til staðar í Jemen, en heilbrigðiskerfi þeirra er nú við það að hrynja, enn fleiri hafa fallið undir fátæktarmörk en áður og þjást af hungri og vegna lokana skólastofnanna hafa mörg þúsund börn ekki aðgang að mentun.</p>
-        <div className="shape">
-        </div>
+        
       </div>
     );
   }
--- yemen-action-2020/src/components/Disclaimer.js
+++ yemen-action-2020/src/components/Disclaimer.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import "../componentStyles/disclaimer.scss";
+import "animate.css/animate.min.css";
+import ScrollAnimation from 'react-animate-on-scroll';
 
 class Disclaimer extends Component {
   render() {
     return <div className="disclaimer">
-      <h3>Hugmyndalegur fyrirvari</h3>
-      <p>Í dag þjáist þriðja hver manneskja, eða 2.6 milljarðar fólks á heimsvísu, af einhvers konar vannæringu [Food Aid Foundation]. Fyrir langflest þessara einstaklinga er hungur ekki val. Það er því nauðsynlegt fyrir afmarkaðan hóp Íslendinga að vera meðvitaður um eigin forréttindum, og líta tilraunir sínar með svengd, ekki sömu augum og áralanga hungursneyð jemensku þjóðarinnar, sem um þessar mundir stendur í borgarstyrjöld og tekst á við heimsfaraldur, ofan á fátækt, ofbeldi, og veikbyggt heilbrigðiskerfi.</p>
-      <h3>Heilsufarslegur fyrirvari</h3>
-      <p>Þátttaka í áskoruninni er á ábyrgð einstaklinga. Hvatning til þátttöku nær vitanlega ekki til þeirra sem eiga á hættu að skaða líkamlega eða andlega heilsu sína. Þvert á móti ráðum einstaklingum frá því að taka þátt ef þá hrjá undirliggjandi sjúkdómar eða óheilbrigt samband við fæðu. Aldrei skal gera stórtækar breytingar á matarvenjum án þess að ráðfæra sig við lækni fyrst.</p>
+      <ScrollAnimation animateIn='animate__animated animate__fadeInLeft'>
+        <h3>Hugmyndalegur fyrirvari</h3>
+        <p>Í dag þjáist þriðja hver manneskja, eða 2.6 milljarðar fólks á heimsvísu, af einhvers konar vannæringu [Food Aid Foundation]. Fyrir langflest þessara einstaklinga er hungur ekki val. Það er því nauðsynlegt fyrir afmarkaðan hóp Íslendinga að vera meðvitaður um eigin forréttindum, og líta tilraunir sínar með svengd, ekki sömu augum og áralanga hungursneyð jemensku þjóðarinnar, sem um þessar mundir stendur í borgarstyrjöld og tekst á við heimsfaraldur, ofan á fátækt, ofbeldi, og veikbyggt heilbrigðiskerfi.</p>
+      </ScrollAnimation>
+      <ScrollAnimation animateIn="animate__animated animate__fadeInRight">
+        <h3>Heilsufarslegur fyrirvari</h3>
+        <p>Þátttaka í áskoruninni er á ábyrgð einstaklinga. Hvatning til þátttöku nær vitanlega ekki til þeirra sem eiga á hættu að skaða líkamlega eða andlega heilsu sína. Þvert á móti ráðum einstaklingum frá því að taka þátt ef þá hrjá undirliggjandi sjúkdómar eða óheilbrigt samband við fæðu. Aldrei skal gera stórtækar breytingar á matarvenjum án þess að ráðfæra sig við lækni fyrst.</p>
+      </ScrollAnimation>
     </div>;
   }
 }
