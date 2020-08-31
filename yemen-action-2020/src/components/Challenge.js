0131807d9176b28c29a6c047cb8c2209e9471efb
Drifa Atladottir <drifaatladottir@Drifas-MacBook-Pro.local>
2020-08-31 16:58:09 +0000

    setti inn mynd fyrir challengið

--- yemen-action-2020/src/components/Challenge.js
+++ yemen-action-2020/src/components/Challenge.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 import "../componentStyles/challenge.scss";
+import instastory from "../images/instastory.png"
+import "animate.css/animate.min.css";
+import ScrollAnimation from 'react-animate-on-scroll';
+
 
 class Challenge extends Component {
   render() {
     return <div className="challenge">
-      <div className="challengeText">
-        <h2>VIÐ SKORUM Á ÞIG</h2>
-        <p>Yfirgnæfandi meirihluti Íslendinga upplifir ekki hungur í daglegu lífi. Samt sem áður erum við stöðugt að „deyja úr hungri“. En hvernig er að upplifa raunverulegt hungur, umfram almenna svengd, líkt og 20 milljónir Jemena gera dag hvern? Við skorum á þig að prófa!</p>
-        <p> Við biðjum ungt fólk á Íslandi að sleppa máltíð, annað hvort (pulsa og kók) … (apótek) …., og styrkja börn í Jemen um fjárhæðina sem henni nemur. Jafnframt biðjum við fólk að íhuga reynslu sína, og deila upplifuninni á InstaStory  með myllumerkinu #deyjaúrhungri. Þannig vekjum við aðra til umhugsunar um efnahagsleg forréttindi sín, og söfnum pening fyrir jemensk börn.</p>
-      </div>
+        <h2 className
+        ="challengeTitle">VIÐ SKORUM Á ÞIG</h2>
+        <div className="challengeContent">
+        <ScrollAnimation animateIn='animate__animated animate__fadeInUp' className="animation">
+          <div className="challengeImage">
+            <img src={instastory} alt="instastory" width="200" height="200"></img>
+          </div>
+        </ScrollAnimation>
+          <div className="challengeText">
+            <p>Yfirgnæfandi meirihluti Íslendinga upplifir ekki hungur í daglegu lífi. Samt sem áður erum við stöðugt að „deyja úr hungri“. En hvernig er að upplifa raunverulegt hungur, umfram almenna svengd, líkt og 20 milljónir Jemena gera dag hvern? Við skorum á þig að prófa!</p>
+            <p> Við biðjum ungt fólk á Íslandi að sleppa máltíð, annað hvort (pulsa og kók) … (apótek) …., og styrkja börn í Jemen um fjárhæðina sem henni nemur. Jafnframt biðjum við fólk að íhuga reynslu sína, og deila upplifuninni á InstaStory  með myllumerkinu #deyjaúrhungri. Þannig vekjum við aðra til umhugsunar um efnahagsleg forréttindi sín, og söfnum pening fyrir jemensk börn.</p>
+          </div>
+        </div>
     </div>;
 
   }
