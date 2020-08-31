19cd10c21de3f0bc9a582b162e711c894d40e346
Drifa Atladottir <drifaatladottir@Drifas-MacBook-Pro.local>
2020-08-31 18:13:11 +0000

    laga villu

--- yemen-action-2020/src/components/Header.js
+++ yemen-action-2020/src/components/Header.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import "../componentStyles/header.scss";
+import instalogo from "../images/instalogo-white.png"
+import menuitem from "../images/menu-white.png"
+
 
 class Header extends Component {
   render() {
     return (
       <div className="header">
-        <p>Header</p>
+        <div>
+            <img className="instalogo" src={instalogo} alt="instagram logo"></img>
+        </div>
+        <div>
+            <img className="menuitem" src={menuitem} alt="menu item"></img>
+        </div>
     </div>
     );
   }
